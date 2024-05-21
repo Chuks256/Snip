9ef41b42b3485e674d9bfe4ba47daead6a1587ff
chuks <alexanderchuks310@gmail.com>
2024-05-21 10:23:50 +0100

    done

--- config/db_config.js
+++ config/db_config.js
@@ -1,13 +1,8 @@
-
+require("dotenv").config();
 let sql_module=require("mysql2");
 
-//  define sql environment params 
-let sql_env_params={
-    uri:"mysql://avnadmin:AVNS_eD1DYAE6cdBRmxaIc8-@mysql-b72bfe8-alexanderchuks310-041a.d.aivencloud.com:23928/defaultdb?ssl-mode=REQUIRED"
-}
-
 // create clustered pool connection
-let define_db_connection=sql_module.createConnection(sql_env_params);
+let define_db_connection=sql_module.createConnection(process.env.URI);
 define_db_connection.connect(err =>{
     if(err){
         throw new Error(err)
