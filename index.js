753e5366cefdffb777fbd9fbc13d4febb53a6d6d
chuks <alexanderchuks310@gmail.com>
2024-05-21 05:41:59 +0100

    done with this shit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let defined_route_module=new route_Module();
 //  define api routes 
 app.get("/api/snip_url",defined_route_module.snip_url);
 app.get("/api/getOriginalUrl/:reference_id",defined_route_module.getOriginalUrl);
-app.get("/api/test",async(req,res)=>{res.json({msg:"api is working fine"})} )
+app.get("/api/test",(req,res)=>{res.json({msg:"api is working fine"})} )
 app.get("/api/getAllData",defined_route_module.getAllData)
 // config api server 
 app.listen(4005,async()=>{
