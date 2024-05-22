c4c3de0160707643a6000035d26c69c2a91c7ede
chuks <alexanderchuks310@gmail.com>
2024-05-22 09:16:25 +0100

    still fixing bug

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ let defined_route_module=new route_Module();
 app.get("/api/snip_url",defined_route_module.snip_url);
 app.get("/api/getOriginalUrl/:reference_id",defined_route_module.getOriginalUrl);
 app.get("/api/test",(req,res)=>{res.json({msg:"api is working fine"})} )
+app.get("/check_status",(req,res)=>{res.json({status:"Server active "})})
 app.get("/api/getAllData",defined_route_module.getAllData)
 // config api server 
 
