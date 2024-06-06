3efb5e28cb69c886ddd68898f0e49254a70dd53d
chuks <alexanderchuks310@gmail.com>
2024-06-06 10:02:23 +0100

    done fixing bugs

--- db_config/dbconfig.js
+++ db_config/dbconfig.js
@@ -2,6 +2,16 @@ require("dotenv").config();
 let sql_module=require("mysql2");
 
 // create clustered pool connection
+// process.env.URI
+
+const db_config={
+    user:"root",
+    host:"localhost",
+    database:"defaultdb",
+    password:"root",
+    port:3306
+}
+
 let define_db_connection=sql_module.createConnection(process.env.URI);
 define_db_connection.connect(err =>{
     if(err){
